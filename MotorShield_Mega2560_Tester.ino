0ea9e9d0c1133159eabf309a913bacdbd472fc65
sirlopug@gmail.com <sirlopug@gmail.com>
2014-09-02 00:28:39 -0400

    Initial Commit

---
+++ MotorShield_Mega2560_Tester.ino
@@ -0,0 +1,17 @@
+#include <SPI.h>
+
+// MotorShield A Channel
+const int A_DIRECTION = 53;
+const int A_PWM = 51; 
+const int A_BRAKE = 48;
+
+// MotorShield B Channel
+const int B_DIRECTION = 52;
+const int B_PWM = 50;
+const int B_BRAKE = 49;
+
+//MotorShield current sensing
+const int SNS0 = A0;
+const int SNS1 = A1;
+
+
